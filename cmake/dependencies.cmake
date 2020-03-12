dca443768d82d58140b272424876a9e89fc0e542
Igor Egorov <igor@soramitsu.co.jp>
2020-03-12 16:26:41 +0300

    SQLiteModernCpp draft CMake
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,15 +1,5 @@
 # hunter dependencies
 # https://docs.hunter.sh/en/latest/packages/
 
-# https://docs.hunter.sh/en/latest/packages/pkg/GTest.html
-hunter_add_package(GTest)
-find_package(GTest CONFIG REQUIRED)
-find_package(GMock CONFIG REQUIRED)
-
-# https://docs.hunter.sh/en/latest/packages/pkg/Boost.html
-hunter_add_package(Boost)
-find_package(Boost CONFIG REQUIRED)
-
-# https://docs.hunter.sh/en/latest/packages/pkg/Microsoft.GSL.html
-hunter_add_package(Microsoft.GSL)
-find_package(Microsoft.GSL CONFIG REQUIRED)
+# added from hunter_config
+hunter_add_package(sqlite3)
